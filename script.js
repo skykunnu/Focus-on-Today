1ec8e69af2426b1b2730bcff7505c3fb56226a63
Shikhar Khandelwal <skykunnu@gmail.com>
2024-04-12 17:18:33 +0530

    init

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const checkBoxList=document.querySelectorAll('.custom-checkbox');
+
+checkBoxList.forEach((checkbox)=>{
+    checkbox.addEventListener('click',(e)=>{
+        checkbox.parentElement.classList.toggle('completed'); // toggle means it will add the class if not present and remove it when it is present. 
+    })
+})
