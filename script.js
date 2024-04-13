79c0524f65a5bbf27c3187b5befe5041e4f6e9f0
Shikhar Khandelwal <skykunnu@gmail.com>
2024-04-13 16:29:10 +0530

    init

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const checkBoxList=document.querySelectorAll('.custom-checkbox');
 const inputFields=document.querySelectorAll('.goal-input');
 const errorLabel=document.querySelector('.error-label');
 const progressBar=document.querySelector('.progress-bar');
-
-
-
-
+const progressValue=document.querySelector('.progress-value');
 
 
 checkBoxList.forEach((checkbox)=>{
@@ -15,7 +12,8 @@ checkBoxList.forEach((checkbox)=>{
         })
 
         if(allGoalsAdded){
-            checkbox.parentElement.classList.toggle('completed'); // toggle means it will add the class if not present and remove it when it is present. 
+            checkbox.parentElement.classList.toggle('completed'); // toggle means it will add the class if not present and remove it when it is present.
+            progressValue.style.width="33.33%";
         }
         else{
             progressBar.classList.add('show-error'); // As soon as the condition gets false progress-bar will add this class and the show-error class and red text will be displayed.
@@ -24,9 +22,9 @@ checkBoxList.forEach((checkbox)=>{
     })
 })
 
-// Focus Event on inputFields
+//This forEach(focus event) loop will execute after clicking on input field because you clicked empty input field checkbox due to which error line appear.    
 inputFields.forEach((input)=>{
     input.addEventListener('focus',()=>{
-        progressBar.classList.remove('show-error');
+        progressBar.classList.remove('show-error'); // removing the error line. 
     })
 })
