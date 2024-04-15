bda61e525a35dfed0731c292f1fc88eabb872174
Shikhar Khandelwal <skykunnu@gmail.com>
2024-04-15 23:16:36 +0530

    init

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ const inputFields = document.querySelectorAll(".goal-input");
 const errorLabel = document.querySelector(".error-label");
 const progressBar = document.querySelector(".progress-bar");
 const progressValue = document.querySelector(".progress-value");
+const Fact=document.querySelector(".fact");
+
+const fact=[
+    'Raise the bar by completing your goals!',
+    'Well begun is half done',
+    'Just a step away, Keep going',
+    'Whoa! You just completed all the goals, time for chill :D',
+]
 
 const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {}; // fetching data from local storage and also saving the pre-existing data for future use.
 let completedGoalsCount=Object.values(allGoals).filter((goal)=>goal.completed).length; // calculating the count of how many completed properties are true.(Object.value is converting values of allGoals object into an array).
 progressValue.style.width= `${completedGoalsCount/3 * 100}%`; // whenever our page gets reload, the progress bar should be display on screen. 
 progressValue.firstElementChild.innerText=`${completedGoalsCount}/3 completed`;
-if( progressValue.firstElementChild.innerText=='3/3 completed'){
-    progressValue.firstElementChild.innerText=`🥳🥳 Hurray!! you Completed your Daily Task`;
-  }
+Fact.innerText=fact[completedGoalsCount]; // It is changing facts after the completion of each task.
 
+   if( progressValue.firstElementChild.innerText=='3/3 completed'){
+    progressValue.firstElementChild.innerText=`🥳🥳 Hurray!! You Completed your Daily Task`;
+  }
 
 // Logic for clickevent on checkboxes.
 checkBoxList.forEach((checkbox) => {
@@ -30,9 +39,11 @@ checkBoxList.forEach((checkbox) => {
 
       progressValue.style.width= `${completedGoalsCount/3 * 100}%`; // Counting the percentage completed and using percentage it is increasing and decreasing width. 
       progressValue.firstElementChild.innerText=`${completedGoalsCount}/3 completed`; // Using firstElementChild we are accessing the span tag of progress value. 
+      Fact.innerText=fact[completedGoalsCount];
       if( progressValue.firstElementChild.innerText=='3/3 completed'){
-        progressValue.firstElementChild.innerText=`🥳🥳 Hurray!! you Completed your Daily Task`;
+        progressValue.firstElementChild.innerText=`🥳🥳 Hurray!! You Completed your Daily Task`;
       }
+
       localStorage.setItem("allGoals", JSON.stringify(allGoals)); // to store above statement in the local storage. 
 
     } else {
@@ -42,8 +53,7 @@ checkBoxList.forEach((checkbox) => {
 });
 
 // Logic for inserting value into inputField and also storing it in local storage.
-inputFields.forEach((input) => {
-  // this input is pointing to each element of inputFields array.
+inputFields.forEach((input) => { // this input callback is pointing to each element of inputFields array.
 
   input.value = allGoals[input.id].name; // to display on the web-app Screen.
 
@@ -56,9 +66,17 @@ inputFields.forEach((input) => {
   input.addEventListener("focus", () => {
     progressBar.classList.remove("show-error"); // removing the error line as soon as we click on inputFields this event will be triggered.
   });
-
-  // This is the Logic of inserting input value into allGoals object and also storing input values into local storage.
+  // (e.target.value & input.value both are same).
+  // This is the Logic of inserting input values into allGoals object and also storing input values into local storage.
   input.addEventListener("input", (e) => {
+
+    //logic is for making input field non editable after completion of task. 
+    if (allGoals[input.id].completed) {
+        input.value=allGoals[input.id].name;
+        return ; // without it will update the value even after completion. 
+      }
+
+
     allGoals[input.id] =
       // this object has been created to store name and completed property.
       {
